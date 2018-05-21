d65c70e0122c0160d07d6c5ff8d8ceee2dc7d289
Renato Oliveira <ren811@gmail.com>
2018-05-20 21:58:25 -0300

    Added option to format output.

--- spotify_status.py
+++ spotify_status.py
@@ -6,21 +6,44 @@ import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-t', '--trunclen', type=int, metavar='trunclen')
+parser.add_argument(
+    '-t',
+    '--trunclen',
+    type=int,
+    metavar='trunclen'
+)
+parser.add_argument(
+    '-f',
+    '--format',
+    type=str,
+    metavar='custom format',
+    dest='custom_format'
+)
 args = parser.parse_args()
 
+# Default parameters
+output = '{artist}: {song}'
+trunclen = 25
+
+# parameters can be overwritten by args
 if args.trunclen is not None:
     trunclen = args.trunclen
-else:
-    trunclen = 25
+if args.custom_format is not None:
+    output = args.custom_format
 
 try:
     session_bus = dbus.SessionBus()
-    spotify_bus = session_bus.get_object("org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2")
+    spotify_bus = session_bus.get_object(
+        'org.mpris.MediaPlayer2.spotify',
+        '/org/mpris/MediaPlayer2'
+    )
 
-    spotify_properties = dbus.Interface(spotify_bus, "org.freedesktop.DBus.Properties")
+    spotify_properties = dbus.Interface(
+        spotify_bus,
+        'org.freedesktop.DBus.Properties'
+    )
 
-    metadata = spotify_properties.Get("org.mpris.MediaPlayer2.Player", "Metadata")
+    metadata = spotify_properties.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
 
     artist = metadata['xesam:artist'][0]
     song = metadata['xesam:title']
@@ -30,15 +53,15 @@ try:
         song += '...' 
         if ('(' in song) and (')' not in song):
             song += ')'
-    output = artist + ': ' + song
     
     # Python3 uses UTF-8 by default. 
     if sys.version_info.major == 3:
-        print(output)
+        print(output.format(artist=artist, song=song))
     else:
-        print(output.encode('UTF-8'))
+        print(output.format(artist=artist, song=song).encode('UTF-8'))
 except Exception as e:
     if isinstance(e, dbus.exceptions.DBusException):
-        print("")
+        print('')
     else:
         print(e)
+
