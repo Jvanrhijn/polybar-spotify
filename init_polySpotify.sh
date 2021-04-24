caed7a5fe4bd23752b9ed7ca9a772e352ea8f396
emc-re-ds <83116526+emc-re-ds@users.noreply.github.com>
2021-04-24 17:57:15 +0200

    Update init_polySpotify.sh

--- init_polySpotify.sh
+++ init_polySpotify.sh
@@ -3,6 +3,6 @@
 # initialize polybar-spotify scroll if the process exists
 PLAYER=$(playerctl metadata --format '{{playerName}}' 2>&1)
 if [ "$PLAYER" = "spotify" ]; then
-    python -u /path/to/spotify/script -s -t 35 -f '{artist}: {song}' -p ' , '
+    python -u /path/to/spotify/script -s -t 35 -f '{artist}: {song}' -p '[playing],[paused]'
 fi
 
